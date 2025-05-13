52f95eea790da1bdaf4a98a59e8630598eff4cc9
Rohit <rohit_ghosh@epam.com>
2025-05-13 15:50:33 +0530

    fix

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://us6r3evmsi.execute-api.ap-northeast-2.amazonaws.com/dev",
+  baseURL: "https://localhost:3000/api",
   headers: {
     "Content-Type": "application/json",
   },
